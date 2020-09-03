8c93685a124663918a646949aa3e5465b3c74a75
smisachsen <stian.m.isachsen@gmail.com>
2020-09-03 14:52:05 +0200

    added argparser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sympy
+import argparse
 from tensorforce import Agent, Runner
 
 from rayleigh_benard_environment import RayleighBenardEnvironment
@@ -10,9 +11,17 @@ from config import *
 """
 
 
+parser = argparse.ArgumentParser(description = "Set num_episodes, max_episode_timesteps")
+parser.add_argument("NUM_EPISODES", help = "number of episodes to run",
+    type = int)
+parser.add_argument("MAX_EPISODE_TIMESTEPS", help = "number of timesteps per episode before termination",
+    type = int)
+
+args = parser.parse_args()
+
 envs = [RayleighBenardEnvironment(
     num_dt_between_actions = NUM_DT_BETWEEN_ACTIONS,
-    max_episode_timesteps = MAX_EPISODE_TIMESTEPS,
+    max_episode_timesteps = args.MAX_EPISODE_TIMESTEPS,
     num_state_points = NUM_STATE_POINTS,
     num_actions = NUM_ACTIONS,
     RB_config = RB_CONFIG) for _ in range(NUM_ENVS)]
@@ -20,7 +29,7 @@ envs = [RayleighBenardEnvironment(
 """
 #### SETUP AGENT ####
 """
-run_agents(AGENTS_JSON, num_episodes = NUM_EPISODES,
+run_agents(AGENTS_JSON, num_episodes = args.NUM_EPISODES,
     agent_names = AGENT_NAMES, environments = envs,
     output = True, save_directory = "data/")
 
