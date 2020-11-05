ef4b2ad15f9d01783b97fa8b9e3c20c22926ce78
smisachsen <stian.m.isachsen@gmail.com>
2020-11-05 19:49:47 +0100

    added args ro runner

--- launch_parallel_training.py
+++ launch_parallel_training.py
@@ -41,30 +41,31 @@ for crrt_simu in range(number_servers):
         timing_print=(crrt_simu == 0)
     ))
 
-if use_best_model:
-    evaluation_environment = environments.pop()
-else:
-    evaluation_environment = None
+# if use_best_model:
+#     evaluation_environment = environments.pop()
+# else:
+#     evaluation_environment = None
 
 network = [dict(type='dense', size=512), dict(type='dense', size=512)]
 
 agent = Agent.create(
     # Agent + Environment
-    agent='ppo', environment=example_environment,
+    agent='ppo', environment=example_environment, max_episode_timesteps=MAX_EPISODE_TIMESTEPS,
     # TODO: nb_actuations could be specified by Environment.max_episode_timesteps() if it makes sense...
     # Network
     network=network,
     # Optimization
     batch_size=20, learning_rate=1e-3, subsampling_fraction=0.2, optimization_steps=25,
     # Reward estimation
-    likelihood_ratio_clipping=0.2,  # ???
+    likelihood_ratio_clipping=0.2,
+    estimate_terminal=True,
     # TensorFlow etc
     parallel_interactions=len(environments), #in the case of use_best_model = True
     saver=dict(directory=os.path.join(os.getcwd(), 'saver_data'))
 )
 
 runner = ParallelRunner(
-    agent=agent, environments=environments, evaluation_environment=evaluation_environment, save_best_agent=use_best_model
+    agent=agent, environments=environments, num_parallel=len(environments)
 )
 
 cwd = os.getcwd()
@@ -73,7 +74,7 @@ sys.path.append(cwd + evaluation_folder)
 # out_drag_file = open("avg_drag.txt", "w")
 
 runner.run(
-    num_episodes=200
+    num_episodes=200, sync_episodes=True
     )
 runner.close()
 
